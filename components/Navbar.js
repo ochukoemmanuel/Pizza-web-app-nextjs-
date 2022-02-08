dbd22351bd5f9cd9e3e2916acedf82c1bf2a8b13
Emmy <ochukoemmanuel07@gmail.com>
2022-02-08 12:00:02 +0100

    Footer page, order page, cart page, single product page, responsive layout

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '../styles/Navbar.module.css';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
@@ -11,7 +12,9 @@ const Navbar = () => {
         </div>
         <div className={styles.texts}>
           <div className={styles.text}>ORDER NOW!</div>
-          <div className={styles.text}>983 405 444</div>
+          <div className={styles.text}>
+            <Link href='call:8998787'>983 405 444</Link>
+          </div>
         </div>
       </div>
       <div className={styles.item}>
@@ -19,7 +22,15 @@ const Navbar = () => {
           <li className={styles.listItem}>Homepage</li>
           <li className={styles.listItem}>Products</li>
           <li className={styles.listItem}>Menu</li>
-          <Image src='/img/logo.png' alt='' width='160px' height='69px' />
+          <Link href='/'>
+            <Image
+              className={styles.pointer}
+              src='/img/logo.png'
+              alt=''
+              width='160px'
+              height='69px'
+            />
+          </Link>
           <li className={styles.listItem}>Events</li>
           <li className={styles.listItem}>Blog</li>
           <li className={styles.listItem}>Contact</li>
@@ -27,7 +38,9 @@ const Navbar = () => {
       </div>
       <div className={styles.item}>
         <div className={styles.cart}>
-          <Image src='/img/cart.png' alt='' width='30px' height='30px' />
+          <Link href='/Cart'>
+            <Image src='/img/cart.png' alt='' width='30px' height='30px' />
+          </Link>
           <div className={styles.counter}>2</div>
         </div>
       </div>
--- components/PizzaCard.js
+++ components/PizzaCard.js
@@ -1,8 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import styles from '../styles/PizzaCard.module.css';
 
 const PizzaCard = () => {
-  return <div style={styles.container}>pizza</div>;
+  return (
+    <div className={styles.container}>
+      <Link href='/product/1'>
+        <Image src='/img/pizza.png' alt='' width='500' height='500' />
+      </Link>
+      <Link href='/product/1'>
+        <h1 className={styles.title}>Fiora pizza</h1>
+      </Link>
+      <Link href='/product/1'>
+        <span className={styles.price}>$19.9</span>
+      </Link>
+      <Link href='/product/1'>
+        <p className={styles.dec}>Lorem ipsum fhdff df dg dgdg dggdgd.</p>
+      </Link>
+    </div>
+  );
 };
 
 export default PizzaCard;
